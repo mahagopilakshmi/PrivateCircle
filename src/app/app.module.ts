f248f8f90ddf65ef9b9350a86602220cb981a5af
maha2506 <mahalakshmi.t@moveinsync.com>
2021-01-29 20:34:11 +0530

    Assignment screens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // MatPaginatorModule,
+    MatTableModule,
+    // MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/json.ts
@@ -0,0 +1,8 @@
+export class Json {
+    date: string;
+    list_name: string;
+    no_of_entities: number;
+    details: String[];
+    selected: boolean;
+    id: number;
+}
